a08a94cf903543120432a1938a7234a90f9a6f42
ac40 <aaron.c.richter@icloud.com>
2023-03-12 16:45:13 +0100

    Build out nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'nav-bg': '#0f172a',
+        "nav-a": "#64748b",
+        "nav-a-hover": "#cbd5e1",
+        "millers-red": "#BE301A",
+        "millers-yellow": "#FAEA0D",
+        "millers-gold": "#BC9404",
+      },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
