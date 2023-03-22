2efe2a7be66f46daaa102ad79b44c508347974a4
ac40 <aaron.c.richter@icloud.com>
2023-03-22 21:26:58 +0100

    Add content to index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [
-    tailwind(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
