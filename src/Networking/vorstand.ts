2b60195076fdd0a02813971742aa77a47bd315c6
ac40 <aaron.c.richter@icloud.com>
2023-04-22 21:23:38 +0200

    Add vorstand page

---
+++ src/Networking/vorstand.ts
@@ -0,0 +1,18 @@
+interface VorstandsMitglied {
+    name: string
+    email: string
+    role: string
+    phone: string
+}
+
+export function getVorstandsMitglieder(json: [any]): VorstandsMitglied[] {
+    var members: VorstandsMitglied[] = []
+
+    for (var i = 0; i < json.length; i++) {
+        members.push({ name: json[i].acm_fields.name, email: json[i].acm_fields.email, role: json[i].acm_fields.rolle, phone: json[i].acm_fields.telefonnummer} as VorstandsMitglied)
+    }
+
+    console.log("After loop: " + members)
+
+    return members
+}
