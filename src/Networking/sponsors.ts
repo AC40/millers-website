6725faf557ffc16d6a6e93e22591711531c53173
ac40 <aaron.c.richter@icloud.com>
2023-11-12 17:22:49 +0100

    Add fischbecker as sponsor

--- src/Networking/sponsors.ts
+++ src/Networking/sponsors.ts
@@ -5,3 +5,8 @@ export const baseballMinisterText = baseballMinisterTextData[0].acm_fields.text
 
 const westfalenbandeTextData = await (await fetch(wpEndpoint + "websitetexte?slug=westfalenbande-sponsor-beschreibung")).json()
 export const westfalenbandeText = westfalenbandeTextData[0].acm_fields.text
+
+const fischbäckerTextData = await (await fetch(wpEndpoint + "websitetexte?slug=fischbecker-sponsor-text")).json()
+console.log(fischbäckerTextData);
+
+export const fischbäckerText = fischbäckerTextData[0].acm_fields.text
